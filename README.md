8cb60cfd4a3e4d879ab135e1776aa635297b1d87
Andrew Motko <andrew.motko@gmail.com>
2024-02-20 22:05:28 +0200

    init: deploy

---
+++ README.md
@@ -0,0 +1,10 @@
+## Preparing for work
+
+1. Make sure that the LTS version of Node.js is installed on the computer.
+   [Download and install](https://nodejs.org/en/) if necessary.
+2. Install the project's basic dependencies using the `npm install` command.
+3. Start the development mode, execute the `npm start` command.
+4. Go to [http://localhost:3000](http://localhost:3000) in the browser. This
+   page will automatically reload after saving the changes project files.
+
+---
---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+import SignIn from 'components/SignInForm/SignIn';
+
+export const App = () => {
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>Title 1</Tab>
+        <Tab>Sign In Form</Tab>
+      </TabList>
+
+      <TabPanel>
+        <h2>Any content 1</h2>
+      </TabPanel>
+      <TabPanel>
+        <SignIn />
+      </TabPanel>
+    </Tabs>
+  );
+};
